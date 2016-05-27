7624d8b9a82ae6ed733141ee43df61edb54020ad
Hamed Najafabadi <hsnajafabadi@users.noreply.github.com>
2016-05-27 11:15:31 -0400

    Updated graphics
    Improved graphics for heatmaps.

--- src/_R/REMBRANDTS.R
+++ src/_R/REMBRANDTS.R
@@ -29,15 +29,18 @@ merged <- merge( merged, exonRawCounts[,c(1,ncol(exonRawCounts))],by="GeneID")
 merged <- merge( merged, intronRawCounts[,c(1,ncol(intronRawCounts))],by="GeneID")
 
 # optimize the total read count threshold so as to maximize correlation between exon and intron fold-changes
+print( paste( "Optimizing read count cutoff at stringency ", stringency, " ...", sep="" ) )
 correl_all <- cor(
 	unlist(merged[ , 2:(nSample+1)]),
 	unlist(merged[ , (nSample+2):(2*nSample+1)]) )
 print( paste( "Total correlation is ", correl_all, sep="" ) )
+print( paste( "Total number of genes is ", nrow(merged), sep="" ) )
 	
 correl_max <- -10
-for( i in seq(50,0,by=-0.1))
-	if( sum(merged$MedianIntron > i & merged$MedianExon > i) > 500 &
-		sum(merged$MedianIntron <= i+0.1 & merged$MedianExon <= i+0.1) > 0 )
+cutoff_table <- data.frame(matrix(c(Inf,0,NA),nrow=1,ncol=3,dimnames=list("",c("Threshold","NumGenes","Correlation"))))
+cutoffs <- quantile( c(merged$MedianIntron, merged$MedianExon), probs = seq( 1, 0, -0.01 ) )
+for( i in cutoffs )
+	if( sum(merged$MedianIntron > i & merged$MedianExon > i) > 500 )
 	{	
 		correl <- cor(
 			unlist(merged[ merged$MedianIntron > i & merged$MedianExon > i , 2:(nSample+1)]),
@@ -49,12 +52,12 @@ for( i in seq(50,0,by=-0.1))
 		if( correl >= (correl_max-correl_all)*stringency + correl_all )
 			threshold <- i
 
-		print( paste( i, correl, sum( merged$MedianIntron > i & merged$MedianExon > i) ) )
-  
+		cutoff_table <- rbind( cutoff_table, c( i, sum( merged$MedianIntron > i & merged$MedianExon > i), correl ) );  
 	}
 
 print( paste( "Maximum correlation is ", correl_max, sep="" ) )
 print( paste( "Selected threshold is ", threshold, sep="" ) )
+print( paste( "Number of remaining genes is ", sum(merged$MedianIntron > threshold & merged$MedianExon > threshold), sep="" ) )
 
 # the filtered exon counts
 y <- unlist(merged[ merged$MedianIntron > threshold & merged$MedianExon > threshold, 2:(nSample+1)])
@@ -64,6 +67,8 @@ x <- unlist(merged[ merged$MedianIntron > threshold & merged$MedianExon > thresh
 jpeg(file=paste(outputFolder,"/scatterplot.jpg",sep=""),
 	width=300,height=350)
 smoothScatter( x, y, colramp=colorRampPalette(c( "white", "red", "black" ) ), nbin=400, nrpoints=0, xlab="Log2 fold-change of intronic reads", ylab="Log2 fold-change of exonic reads" )
+lines( c(-10,+10), c(0,0), lty=3 )
+lines( c(0,0), c(-10,+10), lty=3 )
 
 # The intron counts represent the effect of transcriptional changes
 # The exon counts represent the combined effect of transcriptional and post-transcriptional changes
@@ -74,21 +79,21 @@ smoothScatter( x, y, colramp=colorRampPalette(c( "white", "red", "black" ) ), nb
 exon.counts <- merged[ merged$MedianIntron > threshold & merged$MedianExon > threshold, 1:(nSample+1)]
 # plot the similarity heatmap for exon counts
 sim <- cor(as.matrix(exon.counts[,2:(nSample+1)]))
-width  <- ncol(sim) * 36.9 + 231
-height <- nrow(sim) * 36.9 + 231
+width  <- ncol(sim) * 15 + 200
+height <- nrow(sim) * 15 + 200
 jpeg(file=paste(outputFolder,"/exonic.filtered.correl.heatmap.jpg",sep=""),
 	width=width,height=height)
-heatmap.2(1-sim, distfun=as.dist, trace="none", margin=c(13, 13),symm=T,revC=T,breaks=seq(0,2,length.out=256),col=colorRampPalette(c("red","white","blue"))(255))
+heatmap.2(sim, distfun=function(x) as.dist(1-x), trace="none", margin=c(10, 10),symm=T,revC=T,breaks=seq(-1,1,length.out=256),col=colorRampPalette(c("blue","white","red"))(255), key.title=NA, key.xlab="Pearson correlation", density.info="none", key.ylab=NA, keysize=0.85)
 
 # create a copy of intron counts
 intron.counts <- merged[ merged$MedianIntron > threshold & merged$MedianExon > threshold, c(1,(nSample+2):(2*nSample+1))]
 # plot the similarity heatmap for intron counts
 sim <- cor(as.matrix(intron.counts[,2:(nSample+1)]))
-width  <- ncol(sim) * 36.9 + 231
-height <- nrow(sim) * 36.9 + 231
+width  <- ncol(sim) * 15 + 200
+height <- nrow(sim) * 15 + 200
 jpeg(file=paste(outputFolder,"/intronic.filtered.correl.heatmap.jpg",sep=""),
 	width=width,height=height)
-heatmap.2(1-sim, distfun=as.dist, trace="none", margin=c(13, 13),symm=T,revC=T,breaks=seq(0,2,length.out=256),col=colorRampPalette(c("red","white","blue"))(255))
+heatmap.2(sim, distfun=function(x) as.dist(1-x), trace="none", margin=c(10, 10),symm=T,revC=T,breaks=seq(-1,1,length.out=256),col=colorRampPalette(c("blue","white","red"))(255), key.title=NA, key.xlab="Pearson correlation", density.info="none", key.ylab=NA, keysize=0.85)
 
 # sample-by-sample, deconvolute the PTR effect
 ptr <- exon.counts
@@ -100,13 +105,14 @@ for( i in 2:(nSample+1) )
 
 # plot the similarity heatmap for deconvoluted PTR matrix
 sim <- cor(as.matrix(ptr[,2:(nSample+1)]))
-width  <- ncol(sim) * 36.9 + 231
-height <- nrow(sim) * 36.9 + 231
+width  <- ncol(sim) * 15 + 200
+height <- nrow(sim) * 15 + 200
 jpeg(file=paste(outputFolder,"/stability.filtered.correl.heatmap.jpg",sep=""),
 	width=width,height=height)
-heatmap.2(1-sim, distfun=as.dist, trace="none", margin=c(13, 13),symm=T,revC=T,breaks=seq(0,2,length.out=256),col=colorRampPalette(c("red","white","blue"))(255))
+heatmap.2(sim, distfun=function(x) as.dist(1-x), trace="none", margin=c(10, 10),symm=T,revC=T,breaks=seq(-1,1,length.out=256),col=colorRampPalette(c("blue","white","red"))(255), key.title=NA, key.xlab="Pearson correlation", density.info="none", key.ylab=NA, keysize=0.85)
 
 # write the tables
+write.table(cutoff_table,paste(outputFolder,"/cutoff.txt",sep=""),sep="\t",quote=F,row.names=F)
 write.table(exon.counts,paste(outputFolder,"/exonic.filtered.mx.txt",sep=""),sep="\t",quote=F,row.names=F)
 write.table(intron.counts,paste(outputFolder,"/intronic.filtered.mx.txt",sep=""),sep="\t",quote=F,row.names=F)
 write.table(ptr,paste(outputFolder,"/stability.filtered.mx.txt",sep=""),sep="\t",quote=F,row.names=F)
